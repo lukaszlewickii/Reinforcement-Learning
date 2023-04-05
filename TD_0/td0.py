6914fe9318a54abf933642757963b934ce83a064
lukaszlewickii <lukasz.lewicki@edu.uekat.pl>
2023-04-05 16:08:03 +0200

    modify alpha and gamma

--- TD_0/td0.py
+++ TD_0/td0.py
@@ -21,7 +21,7 @@ parser.add_argument('-g', '--gamma',
 
 parser.add_argument('-a', '--alpha',
                     type=float,
-                    default=0.05,
+                    default=0.5,
                     help='Alpha parameter for td target formula. Default: 0.05'
                    )
 
@@ -35,11 +35,39 @@ parser.add_argument('-r', '--render',
 env = gym.make('Walking5-v0')
 pi = lambda x: np.random.randint(2) # uniform random policy
 
-def td(pi, env, gamma=1.0, alpha=0.05, n_episodes=10, render=True):
+def td(pi, env, gamma=1.0, alpha=0.5, n_episodes=10, render=True):
     # ENTER YOUR CODE HERE
     # You should return the vector of state values V
-    V = None
+    V = np.array([0] + [0.5] * (env.observation_space.n - 2) + [0])
+    
+    n_visited = np.array([1] * env.observation_space.n)
+    
+    for t in range(n_episodes):
+        state, info = env.reset()
+        n_visited[state] += 1
+        terminal = False
+        
+        visited_states = []
+        rewards = []
+        
+        if render:
+            env.render(V)
+            
+        while not terminal:
+            visited_states.append(state)
+            action = pi(state)
+            next_state, reward, terminal, truncated, info = env.step(action)
+            V[state] += alpha * (reward + gamma * V[state + 1] - V[state])
+            
+            state = next_state
+            n_visited[state] += 1
+            
+            if render:
+                env.render(V)
+    
     return V
+
+#python td0.py -e 100
 
 if __name__ == "__main__":
     args = parser.parse_args()
