62da509842601b3362a13abf26b1f3c0df6dfbc3
lukaszlewickii <lukasz.lewicki@edu.uekat.pl>
2023-04-19 16:16:52 +0200

    19.04

--- agents.py
+++ agents.py
@@ -18,7 +18,6 @@ class SarsaAgent:
         # Sets the Q_est to a matrix of zeros with the shape of (nS, nA)
         self.Q_est = np.array([np.zeros(env.action_space.n) for _ in range(env.observation_space.n)])
         
-        
     def get_action(self, state):
         """
         Get an action from Q_est using epsilon-greedy policy. Action is
@@ -31,7 +30,12 @@ class SarsaAgent:
         """
         # TODO: Implement the get_action function for SARSA agent.
         # YOUR CODE HERE
-        pass
+        action = 0
+        if np.random.uniform(0, 1) < self.epsilon:
+            action = self.action_space.sample()
+        else:
+            action = np.argmax(self.Q[state, :])
+        return action
         
     def update(self, state, action, reward, next_state, next_action):
         """ Updates the Q_est using the SARSA update rule.
@@ -45,6 +49,10 @@ class SarsaAgent:
         """
         # TODO: Implement the update function for SARSA agent.
         # YOUR CODE HERE
+        
+        predict = self.Q[state, action]
+        target = reward + self.gamma * self.Q[next_state, next_action]
+        self.Q[state, action] += self.alpha * (target - predict)
         pass
     
     def save(self, path = './SARSA_Q_est.npy'):
@@ -60,7 +68,6 @@ class SarsaAgent:
             path (str, optional): Path to the file. Defaults to './SARSA_Q_est.npy'.
         """
         self.Q_est = np.load(path)
-
 
 class QLearningAgent:
     def __init__(self, env, alpha, gamma, epsilon):
@@ -91,9 +98,14 @@ class QLearningAgent:
         Returns:
             action (int): action to take.
         """
-        # TODO: Implement the get_action function for QLearning agent.
-        # YOUR CODE HERE
-        pass
+        # Selects the action with the highest Q-value with probability 1-epsilon
+        if np.random.uniform(0, 1) < 1 - self.epsilon:
+            action = np.argmax(self.Q_est[state])
+        # Selects a random action from the action space with probability epsilon/nA
+        else:
+            action = self.env.action_space.sample()
+        
+        return action
         
     def update(self, state, action, reward, next_state):
         """ Updates the Q_est using the QLearning update rule.
@@ -104,9 +116,10 @@ class QLearningAgent:
             reward (int, float): received reward
             next_state (int): state at time t+1
         """
-        # TODO: Implement the update function for QLearning agent.
-        # YOUR CODE HERE
-        pass
+        # Calculates the temporal difference error (delta)
+        delta = reward + self.gamma * np.max(self.Q_est[next_state]) - self.Q_est[state][action]
+        # Updates the Q-value for the current state-action pair
+        self.Q_est[state][action] += self.alpha * delta
     
     def save(self, path = './QLearning_Q_est.npy'):
         """Saves the Q_est to a file.
